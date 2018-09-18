e12cc0f39c1678476524dd548952e8039d17b9d8
Brendan van Walstijn <brendan.van.walstijn@hva.nl>
2018-09-18 15:44:29 +0200

    Subject
    Client now first asks for a subject. This is used serverside to determine in which file the question should be placed.

--- __main__.py
+++ __main__.py
@@ -1,18 +1,18 @@
-class myServer:
+class myClient:
     import socket  # Import socket module
 
     while True:
+        subject = input("Please enter the subject (C#, c++, CMake, Linux, Compilers)")
+        subject.lower()
         question = input("what is your question?")
 
-        if (question != ''):
-            print('sendin Message!')
+        if (question != '' and subject != ''):
             qt = question.encode()
+            st = subject.encode()
             s = socket.socket()  # Create a socket object
             host = socket.gethostname()  # Get local machine name
             port = 60000  # Reserve a port for your service.
             s.connect((host,port))
-            s.send(qt)
+            s.send("Subject ".encode()+ st + " ".encode() + "Question ".encode() + " ".encode() + qt)
             s.close()
-
-
-
+            print('Question Send!\n')
